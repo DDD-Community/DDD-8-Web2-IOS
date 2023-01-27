171abb4efa7cab1a09b034450fef66e2103b5750
yeonjuan <yeonjuan93@naver.com>
2023-01-27 23:46:57 +0900

    Add Bookmark Screen

--- src/screens/bookmark/bookmark.screen.tsx
+++ src/screens/bookmark/bookmark.screen.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { styles } from "./boolmark.styles";
+import { Text, Layout } from "~components";
+import IconBookmarkBg from "~assets/icon/icon-bookmark-bg.svg";
 
 export const BookmarkScreen = () => {
   return (
-    <View style={styles.view}>
-      <Text>bookmark hear</Text>
-    </View>
+    <Layout style={styles.view} safeAreaStyle={{ width: "100%" }}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitleText}>장소 북마크</Text>
+      </View>
+      <View style={styles.contentView}>
+        <IconBookmarkBg />
+        <Text style={styles.emptyText}>장소를 북마크 해보세요!</Text>
+      </View>
+    </Layout>
   );
 };
--- src/screens/bookmark/boolmark.styles.ts
+++ src/screens/bookmark/boolmark.styles.ts
@@ -1,9 +1,38 @@
 import { StyleSheet } from "react-native";
+import { FontFamily, FontSize, FontWeight, HexColor } from "~constants";
 
 export const styles = StyleSheet.create({
   view: {
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: HexColor.White,
+    height: "100%",
+    width: "100%",
+  },
+  header: {
+    backgroundColor: HexColor.White,
+    height: 44,
+    width: "100%",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+  },
+  headerTitleText: {
+    fontFamily: FontFamily,
+    fontWeight: FontWeight.SemiBold,
+    fontSize: FontSize.Large,
+  },
+  contentView: {
+    backgroundColor: HexColor.N10,
     flex: 1,
+    height: "100%",
     justifyContent: "center",
     alignItems: "center",
+  },
+  emptyText: {
+    color: HexColor.N90,
+    fontWeight: FontWeight.Regular,
+    fontSize: FontSize.Large,
+    marginTop: 22,
   },
 });
